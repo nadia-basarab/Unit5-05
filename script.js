83750c6d933aa09d216ab5cc918f3ac212ea3e55
nadiabasarab <nadia.basarab@ocsbstudent.ca>
2021-03-18 14:48:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function areaofsquare(){
+   var side = document.getElementById("side").value;
+   var output = side*side;
+   document.getElementById("area_status").innerHTML="The area is "+output;
+}
